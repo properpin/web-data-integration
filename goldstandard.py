f1193d0219554e786eca9627d5638734540a6d45
I-Chen Hsieh <46900047+properpin@users.noreply.github.com>
2022-12-03 21:59:31 +0100

    Delete goldstandard.py

--- goldstandard.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-
-
-sevMil = pd.read_csv("companies_sorted.csv")
-ft = pd.read_csv("Forbes Top2000 2017.csv")
-db = pd.read_csv("sparql_2022-10-02_13-24-07Z.csv")
